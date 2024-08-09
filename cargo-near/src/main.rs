3ec378b6d34b3c454b3774de77a16665fe79c61a
dj8yf0μl <noreply@nowhere.org>
2024-08-09 21:35:24 +0300

    add ctx in bin

--- cargo-near/src/main.rs
+++ cargo-near/src/main.rs
@@ -1,10 +1,11 @@
 use cargo_near_lib::build;
+use color_eyre::eyre::Context;
 
 fn main() -> color_eyre::Result<()> {
 
     color_eyre::install()?;
 
-    build(123, 123123)?;
+    build(123, 123123).wrap_err("new context in binary")?;
     println!("hello");
     Ok(())
 
