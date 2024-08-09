5c46f7f44b1e01c7a9eb06ebcebaa971d15d8708
dj8yf0μl <noreply@nowhere.org>
2024-08-09 21:38:09 +0300

    add pic to readme

--- cargo-near/src/main.rs
+++ cargo-near/src/main.rs
@@ -3,7 +3,13 @@ use color_eyre::eyre::Context;
 
 fn main() -> color_eyre::Result<()> {
 
-    color_eyre::install()?;
+    #[cfg(not(debug_assertions))]
+    let display_env_section = false;
+    #[cfg(debug_assertions)]
+    let display_env_section = true;
+    color_eyre::config::HookBuilder::default()
+        .display_env_section(display_env_section)
+        .install()?;
 
     build(123, 123123).wrap_err("new context in binary")?;
     println!("hello");
